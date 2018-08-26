e0e6a4163e7575319f4d79b2c958ad055d0cd4da
mailboxskumar <mailbox.skumar@gmail.com>
2018-08-26 23:08:19 +0530

    updated class file name

--- src/javaprograming/string/Call_ParametrizdConstr_From_DefaultConstr.java
+++ src/javaprograming/string/Call_ParametrizdConstr_From_DefaultConstr.java
@@ -1,19 +1,19 @@
 package javaprograming.string;
 
-public class CallingParametrizdConstrFromDefaultConstr {
+public class Call_ParametrizdConstr_From_DefaultConstr {
 
 	public static void main(String[] args) {
 		
-		CallingParametrizdConstrFromDefaultConstr a = new CallingParametrizdConstrFromDefaultConstr();
+		Call_ParametrizdConstr_From_DefaultConstr a = new Call_ParametrizdConstr_From_DefaultConstr();
 
 	}
 	
-	CallingParametrizdConstrFromDefaultConstr(){
+	Call_ParametrizdConstr_From_DefaultConstr(){
 		this("Calling Parametrzied constructor from Default");
 		System.out.println("Default constructor ");
 	}
 	
-	CallingParametrizdConstrFromDefaultConstr(String str){
+	Call_ParametrizdConstr_From_DefaultConstr(String str){
 		System.out.println("Parameterzied constructor :" + str);
 	}
 	
