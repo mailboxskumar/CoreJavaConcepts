30c5c5167b4f8e0c1327b911e9c7c4ff9d8f187f
Sanjay Kumar <mailbox.skumar@gmail.com>
2020-02-22 19:30:37 +0530

    Array Concept Added

--- src/javacollections/arraylist/ConceptArrayList.java
+++ src/javacollections/arraylist/ConceptArrayList.java
@@ -1,9 +1,41 @@
 package javacollections.arraylist;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ConceptArrayList {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		String url = "https://www.engprod-charter.net/";
+
+		URL obj = new URL(url);
+		HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+		conn.setReadTimeout(5000);
+		conn.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
+		conn.addRequestProperty("User-Agent", "Mozilla");
+		conn.addRequestProperty("Referer", "google.com");
+		
+
+		HttpURLConnection httpconn = new HttpURLConnection(null) {
+			
+			@Override
+			public void connect() throws IOException {
+				
+			}
+			
+			@Override
+			public boolean usingProxy() {
+				return false;
+			}
+			
+			@Override
+			public void disconnect() {
+				
+			}
+		};
 
 	}
 
